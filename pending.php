7f17047ce6dac3cf11232c355cb8fd105ce45097
Projects <hasanattar40@gmail.com>
2024-07-24 12:36:16 +0300

    Update pending.php

--- pending.php
+++ pending.php
@@ -48,7 +48,7 @@ try {
     $mail->Username   = '';                     //SMTP username
     $mail->Password   = '';                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-    $mail->Port       = ;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+    $mail->Port='' ;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
     //Recipients
     $mail->setFrom('', 'Admin');
