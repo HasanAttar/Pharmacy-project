f3a40bcd196712ac92138e76a05a358e0404f938
HasanAttar <hasanattar40@gmail.com>
2024-07-24 11:06:41 +0300

    Update pending.php

--- pending.php
+++ pending.php
@@ -45,10 +45,10 @@ try {
     $mail->isSMTP();                                     //Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'hsnattar2022@gmail.com';                     //SMTP username
-    $mail->Password   = 'gkffmjxhlffrufso';                               //SMTP password
+    $mail->Username   = '';                     //SMTP username
+    $mail->Password   = '';                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+    $mail->Port       = ;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
     //Recipients
     $mail->setFrom('hsnattar2022@gmail.com', 'Admin');
